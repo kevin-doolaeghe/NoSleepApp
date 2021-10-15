8a3deb5929b5f92fa0d8c57f87c5234a5accb2c8
kevin-doolaeghe <doolaeghe.kevin@gmail.com>
2021-10-15 10:43:59 +0200

    Add project files.

---
+++ NoSleepApp/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace NoSleepApp {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(77, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Stop sleep";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Press Ctrl+C to enable sleep.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 65);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "NoSleepApp";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NoSleepApp/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace NoSleepApp {
+    public partial class Form1 : Form {
+        private bool waiting;
+        private BackgroundWorker worker;
+
+        public Form1()
+        {
+            waiting = false;
+            InitializeComponent();
+
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += Wait;
+            worker.ProgressChanged += UpdateCursor;
+            worker.RunWorkerCompleted += EnableButton;
+        }
+
+        private void Wait(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bg = (BackgroundWorker)sender;
+            int i = 0;
+            while (waiting)
+            {
+                bg.ReportProgress(i);
+                i += 10;
+                Thread.Sleep(10);
+            }
+        }
+
+        private void UpdateCursor(object sender, ProgressChangedEventArgs e)
+        {
+
+            int coef = 2;
+            double rad = Math.PI / 180;
+            int i = e.ProgressPercentage;
+            Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(Cursor.Position.X + (int)(coef * Math.Cos(i * rad)), Cursor.Position.Y + (int)(coef * Math.Sin(i * rad)));
+        }
+
+        private void EnableButton(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            waiting = true;
+            worker.RunWorkerAsync();
+            button1.Enabled = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.C && e.Control)
+            {
+                waiting = false;
+                MessageBox.Show("You pressed Ctrl+C");
+            }
+        }
+    }
+}
